3ec16486381ef1656ce31549bb26f29ff2cbb6e8
Hasham <hashamsp36@gmail.com>
2024-05-05 13:39:36 -0400

    master - small lambda and gateway hello endpoint setup

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npm run build`   compile typescript to js
 * `npm run watch`   watch for changes and compile
 * `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npm run deploy`  deploy this stack to your default AWS account/region
+* `npm run destroy`  destroy the deployment
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
+Can view the lambda at your prod link e.g `https://e8np4uwu00.execute-api.us-east-1.amazonaws.com/prod/hello`
--- lib/aws-stack.ts
+++ lib/aws-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class AwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a Lambda function
+    const helloLambda = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'), 
+      handler: 'index.handler',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create an API Gateway
+    const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
+      handler: helloLambda,
+      proxy: false,
+      defaultMethodOptions: {
+        authorizationType: apigateway.AuthorizationType.NONE,
+      },
+    });
+
+    // Define the '/hello' resource with a GET method
+    const helloResource = api.root.addResource('hello');
+    helloResource.addMethod('GET');
   }
 }
